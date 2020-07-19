4f9571c1d2d5efa1e78d566858b4dbb6f1d44501
XinyiYS <michaelxysg@gmail.com>
2020-07-19 21:48:46 +0800

    try to fix a missing sc2map by changing its naming

--- pysc2/maps/mini_games.py
+++ pysc2/maps/mini_games.py
@@ -40,7 +40,7 @@ mini_games = [
     "BuildMarinesWithBarracks",
     "BuildSupplyDepots",
     "BuildSupplyDepotsHard",
-    "BuildMarines_depotbarracks",
+    "BuildMarinesDepotBarracks",
     "BuildBarracks",
     "BuildBarracksHard",
     "BuildMarinesWithBarracksHard",
